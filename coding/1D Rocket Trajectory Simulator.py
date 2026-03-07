c7ea269ed21f0eaa3a1650431c902320ff7f2dd4
Mahir Manazir <mahirmanazir1@gmail.com>
2026-03-07 19:21:26 +0600

    Refactor rocket_launch to plot trajectory
    Updated rocket_launch function to include mass reduction and plotting.

--- coding/1D Rocket Trajectory Simulator.py
+++ coding/1D Rocket Trajectory Simulator.py
@@ -1,20 +1,35 @@
-def rocket_launch(mass, thrust, fuel):
+import matplotlib.pyplot as plt
+def rocket_launch(mass, thrust):
     velocity = 0
     time = 0
     gravity = 9.8
     height = 0
+    heights = []
+    times = []
+    drymass = 0.1*mass
 
     print("3,2,1!")
     print("Launch Initiated!")
-
-    while fuel > 0:
+    
+    while mass > drymass:
         acceleration = thrust/mass - gravity
         velocity = velocity + acceleration
-        height = velocity + height
-        fuel = fuel - 1
-        time = time + 1
+        height = height + velocity
+        time = time + 1 
+        mass = mass - 1 
+        
+        heights.append(height)
+        times.append(time)
+
+    plt.plot(times, heights) 
+    plt.xlabel("Time(s)") 
+    plt.ylabel("Height(y)") 
+    plt.title("Rocket Launch") 
+    plt.show()
 
     print(f"The rocket successfully reached a height of {height}")
     print(f"It took us {time} seconds")
 
-rocket_launch(mass=1000, thrust=10000, fuel=260)
+ 
+
+rocket_launch(mass=2000, thrust=20000)
