e40950fc56fcf4b553c355f1e47185e4d555ba4f
Mahir Manazir <mahirmanazir1@gmail.com>
2026-03-12 20:42:39 +0600

    Refactor rocket launch into Rocket class

--- coding/oneDrocket_simulator(OOP).py
+++ coding/oneDrocket_simulator(OOP).py
@@ -1,38 +1,50 @@
+import numpy as np
 import matplotlib.pyplot as plt
-import numpy as np 
-import math 
+class Rocket(): 
+    def __init__(self, drymass, thrust, fuel):
+        self.drymass = drymass 
+        self.thrust = thrust 
+        velocity = 0 
+        self.times = np.zeros(fuel)
+        self.heights = np.zeros(fuel)
+        self.height = 0
+        self.time = 0
+        self.mass = drymass + fuel
+        self.acceleration = 0
+        self.velocity = 0
+        self.i = 0 
 
-def rocket_launch(mass, thrust):
-    velocity = 0
-    time = 0
-    gravity = 9.8
-    height = 0
-    drymass = 0.1*mass 
-    heights = np.zeros(math.ceil(mass - drymass))
-    times = np.zeros(math.ceil(mass - drymass))
-    i = 0
-
-    print("3,2,1!")
-    print("Launch Initiated!")
-    
-    while mass > drymass:
-        acceleration = thrust/mass - gravity
-        velocity = velocity + acceleration
-        height = height + velocity
-        time = time + 1 
-        mass = mass - 1  
-        heights[i] = height 
-        times[i] = time 
-        i = i + 1
+        while fuel > 0: 
+            self.acceleration = thrust/self.mass - 9.8
+            self.velocity += self.acceleration
+            self.height += self.velocity
+            self.time += 1
+            self.heights[self.i] = self.height
+            self.times[self.i] = self.time
+            fuel -= 1 
+            self.mass -= 1
+            self.i += 1
 
         
 
-    plt.plot(times, heights) 
-    plt.xlabel("Time(s)") 
-    plt.ylabel("Height(y)") 
-    plt.title("Rocket Launch") 
-    plt.show()
+        print(f"Congratulaitons, the rocket reached a height of {self.height} and time of {self.time}")
+        plt.plot(self.times, self.heights)
+        plt.xlabel("Time(s)")
+        plt.ylabel("Height(m))")
+        plt.title("Graphical Representation")
+        plt.show()
 
-    print(f"The rocket successfully reached a height of {height}")
-    print(f"It took us {time} seconds")
-
+        
+        
+    def twr(self):
+        return self.thrust/ (self.drymass*9.8) 
+    
+    def is_powerful (self):
+        if self.twr() > 1:  
+            return True 
+        else: 
+            return False
+    def __str__(self):
+        return f"Mass : {self.drymass}  Thrust: {self.thrust} TWR: {self.twr()}"
+    
+Xrocket = Rocket(500, 150000, 5000)
